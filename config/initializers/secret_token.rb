6451c4323e38d59776496ecf398b28178f488a17
deepthi <deepthirera@gmail.com>
2012-10-10 23:48:50 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrashBoard::Application.config.secret_token = 'd4595bb8e2744f7223b5b2032ee91f3dc632f9498cff8ca7239fd569278002603c99ab04b4eda9a9ad2a9bb7a2f75efa1cc94e5548a74855316c3aa189cb81f4'
